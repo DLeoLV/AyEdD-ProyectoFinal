8e49b07bd6dc8911581189239727083be5e1ff3f
DLeoLV <131694293+DLeoLV@users.noreply.github.com>
2024-06-18 10:42:30 -0500

    Creacion

---
+++ Assets/Scripts/MovimientoFlechas.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoFlechas : MonoBehaviour
+{
+    public float vel = 5f;
+
+    void Update()
+    {
+        float movimientoHorizontal = Input.GetAxis("Horizontal");
+        float movimientoVertical = Input.GetAxis("Vertical");
+
+        Vector3 movimiento = new Vector2(movimientoHorizontal, movimientoVertical) * vel * Time.deltaTime;
+
+        transform.position += movimiento;
+    }
+}
